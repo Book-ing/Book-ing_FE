2e715531c26f946a50f5be65e367771dd171d7af
Taak-e <kjtag0408@gmail.com>
2022-04-29 16:45:48 +0900

    components(ModalCrew): 구현 전 노트생성 페이지 우선작업
    NoteWrite 페이지 작업을 우선하려고  브랜치 이동 전 커밋
    
    2022.04.29 actual project: book-ing
    commit by Taak-e

--- src/pages/Test.jsx
+++ src/pages/Test.jsx
@@ -1,13 +1,27 @@
-import React from "react";
-import Header from "../components/Header";
-import Footer from "../components/Footer";
+import React, {useState} from "react";
+import Cards from "../Components/Cards";
+import { Grid } from "@mui/material";
+import Modal from "react-modal";
+import ModalCrew from "../Components/ModalCrew";
 
 const Test = (props) => {
+
+  const [modalOpen, setModalOpen] = useState(false);
+
   return (
-    <>
-      <Header />
-      <Footer />
-    </>
+    <React.Fragment>
+      <Grid
+        container
+        direction="row"
+        justifyContent="center"
+        alignItems="center">
+      <Cards></Cards>
+      <Cards></Cards>
+      </Grid>
+      <button onClick={() => setModalOpen(true)}>모달 열어새꺄</button>
+        <ModalCrew></ModalCrew>
+
+    </React.Fragment>
   );
 };
 
