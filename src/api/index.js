4e63111b1d754a99b4b7487d1048664f64701466
MinwooPark93 <minwoopark93@gmail.com>
2022-05-05 22:47:51 +0900

    fix(login): Access Token 재발행 작업중/미해결
    Access Token 재발행 수정 작업중 미해결.
    시간관계상 모임페이지 작업 먼저 해놓기 위한 커밋입니다.
    
    2022.05.05 actual project: book-ing
    commit by MinwooPark93

--- src/api/index.js
+++ src/api/index.js
@@ -1,5 +1,6 @@
 import axios from "axios";
-import { cookies } from "../shared/cookie";
+import { cookies, setCookie, getCookie, removeCookie } from "../shared/cookie";
+import history from "../redux/configStore";
 
 const targetServer = "https://sparta-hs.shop/";
 
@@ -39,35 +40,46 @@ export const nonTokenInstance = axios.create({
 
 instance.interceptors.request.use((config) => {
   const accessToken = cookies.get("accessToken");
+  const refreshToken = cookies.get("refreshToken");
   config.headers.common["Authorization"] = `Bearer ${accessToken}`;
+  config.headers.common["refreshToken"] = `${refreshToken}`;
   return config;
 });
 
 requiredInstance.interceptors.request.use((config) => {
   const accessToken = cookies.get("accessToken");
+  const refreshToken = cookies.get("refreshToken");
+  console.log("accessToken", accessToken);
+  console.log("refreshToken", refreshToken);
   config.headers.common["Authorization"] = `Bearer ${accessToken}`;
+  config.headers.common["refreshToken"] = `${refreshToken}`;
   config.headers.common["required"] = 1;
   return config;
 });
 
 formDataInstance.interceptors.request.use((config) => {
   const accessToken = cookies.get("accessToken");
+  const refreshToken = cookies.get("refreshToken");
   config.headers.common["Authorization"] = `Bearer ${accessToken}`;
+  config.headers.common["refreshToken"] = `${refreshToken}`;
   config.headers.common["required"] = 1;
   return config;
 });
 
 nonTokenInstance.interceptors.request.use((config) => {
   const accessToken = cookies.get("accessToken");
+  const refreshToken = cookies.get("refreshToken");
   config.headers.common["Authorization"] = `Bearer ${accessToken}`;
+  config.headers.common["refreshToken"] = `${refreshToken}`;
 
   return config;
 });
 
-// Refresh Token 발급 로직
-let isTokenRefreshing = false;
-let refreshSubscribers = [];
+// Access Token 만료시 로직
+let isTokenRefreshing = false; // Token 재발행중인지 여부
+let refreshSubscribers = []; // axios 요청 배열
 
+// Token 재발행 후
 const onTokenRefreshed = (accessToken) => {
   refreshSubscribers.map((callback, idx) => {
     // console.log(idx + "번째 재요청 완료");
@@ -75,59 +87,147 @@ const onTokenRefreshed = (accessToken) => {
   });
 };
 
+// 배열로 재발행 된 access token을 header 에 넣어 push.
 const addRefreshSubscriber = (callback) => {
   refreshSubscribers.push(callback);
 };
 
-instance.interceptors.response.use(
+requiredInstance.interceptors.response.use(
   (config) => {
+    console.log(config);
+    const status = config.status;
+    const accessToken = config.data.data.accessToken;
+    const refreshToken = config.data.data.refreshToken;
+    // status code가 201 일때,
+    if (status === 201) {
+      if (!isTokenRefreshing) {
+        // 토큰 생성중이라면, if 실행되지 않도록
+        isTokenRefreshing = true; // false 일때는 true로 변경
+        // 재발행 된 access token, refresh token cookie에 저장.
+        setCookie("accessToken", accessToken, {
+          path: "/",
+          sameSite: "None",
+          secure: true,
+        });
+        setCookie("refreshToken", refreshToken, {
+          path: "/",
+          sameSite: "None",
+          secure: true,
+        });
+
+        isTokenRefreshing = false; // 토큰 재발행 상태를 false로 변경
+
+        // axios 기본 header의 authorization token 을 새로 발행된 토큰으로 교체
+        axios.defaults.headers.common.Authorization = `Bearer ${accessToken}`;
+
+        // 현재 요청한 axios의 header의 authorization token 을 새로 발행된 토큰으로교체
+        config.headers.Authorization = `Bearer ${accessToken}`;
+
+        // 첫 요청이 아닌 다른 쌓여있던 요청 다시 요청보내기
+        onTokenRefreshed(accessToken);
+        refreshSubscribers = []; // 요청 배열 초기화
+
+        return axios(config); // 첫 요청 다시 요청
+      }
+      // 기존 요청 재시도
+      const retryOriginalRequest = new Promise((resolve) => {
+        addRefreshSubscriber((accessToken) => {
+          config.headers.Authorization = "Bearer " + accessToken;
+          resolve(axios(config));
+        });
+      });
+      return retryOriginalRequest; // 모아둔 요청 재실행
+    }
     // 오류 없을 때
     return config;
   },
 
   async (error) => {
-    const { config, response } = error;
+    const { response } = error;
     const { status } = response;
-    const originalReq = config; // 에러가 난 요청정보들
 
     if (status === 401) {
-      // 토큰이 없고,
-      if (!isTokenRefreshing) {
-        // 토큰 생성중이면, if 실행되지 않도록
-        isTokenRefreshing = true; // false 일때는 true로 바꿔주고.
-        const refreshToken = cookies.get("refreshToken"); // 리프레시 토큰을 쿠키에서 가져와서
-
-        const { data } = await axios.post(`${targetServer}token`, {
-          refreshToken,
-        });
-
-        const { accessToken: newAccessToken } = data;
-        cookies.set("accessToken", newAccessToken, {
-          path: "/",
-          maxAge: 1800, // 5분
-        }); // 쿠키에 다시 저장
-
-        // console.log("토큰 재생성 완료!"); // 토큰 생성이 완료되면
-        isTokenRefreshing = false; // 토큰 생성중 상태를 fasle로 바꿔주고
-
-        // 헤더를 새로운 액세스토큰으로 헤더 재설정
-        axios.defaults.headers.common.Authorization = `Bearer ${newAccessToken}`;
-        originalReq.headers.Authorization = `Bearer ${newAccessToken}`;
-
-        onTokenRefreshed(newAccessToken); // 첫 요청이 아닌 다른 쌓여있던 요청 다시 요청보내기
-        refreshSubscribers = []; // 요청 배열 초기화
-        // console.log("첫 요청도 다시 요청!");
-        return axios(originalReq); // 첫 요청 다시 요청
-      }
-      // 토큰을 생성중일때  (if문을 passing 한 요청들은 )
-      const retryOriginalRequest = new Promise((resolve) => {
-        addRefreshSubscriber((accessToken) => {
-          originalReq.headers.Authorization = "Bearer " + accessToken;
-          resolve(axios(originalReq));
-        });
+      const accessToken = getCookie("accessToken");
+      const refreshToken = getCookie("refreshToken");
+      removeCookie("accessToken", accessToken, {
+        path: "/",
       });
-      return retryOriginalRequest; // 모아둔 요청 재실행
+      removeCookie("refreshToken", refreshToken, {
+        path: "/",
+      });
+      localStorage.removeItem("userId");
+      localStorage.removeItem("username");
+      localStorage.removeItem("kakaoUserId");
+      history.replace("/login");
+      alert("세션이 만료되었습니다. 다시 로그인 해주세요.");
     }
-    return Promise.reject(error);
   }
 );
+
+// Refresh Token 발급 로직
+// let isTokenRefreshing = false;
+// let refreshSubscribers = [];
+
+// const onTokenRefreshed = (accessToken) => {
+//   refreshSubscribers.map((callback, idx) => {
+//     // console.log(idx + "번째 재요청 완료");
+//     return callback(accessToken);
+//   });
+// };
+
+// const addRefreshSubscriber = (callback) => {
+//   refreshSubscribers.push(callback);
+// };
+
+// instance.interceptors.response.use(
+//   (config) => {
+//     // 오류 없을 때
+//     return config;
+//   },
+
+//   async (error) => {
+//     const { config, response } = error;
+//     const { status } = response;
+//     const originalReq = config; // 에러가 난 요청정보들
+
+//     if (status === 401) {
+//       // 토큰이 없고,
+//       if (!isTokenRefreshing) {
+//         // 토큰 생성중이면, if 실행되지 않도록
+//         isTokenRefreshing = true; // false 일때는 true로 바꿔주고.
+//         const refreshToken = cookies.get("refreshToken"); // 리프레시 토큰을 쿠키에서 가져와서
+
+//         const { data } = await axios.post(`${targetServer}token`, {
+//           refreshToken,
+//         });
+
+//         const { accessToken: newAccessToken } = data;
+//         cookies.set("accessToken", newAccessToken, {
+//           path: "/",
+//           maxAge: 1800, // 5분
+//         }); // 쿠키에 다시 저장
+
+//         // console.log("토큰 재생성 완료!"); // 토큰 생성이 완료되면
+//         isTokenRefreshing = false; // 토큰 생성중 상태를 fasle로 바꿔주고
+
+//         // 헤더를 새로운 액세스토큰으로 헤더 재설정
+//         axios.defaults.headers.common.Authorization = `Bearer ${newAccessToken}`;
+//         originalReq.headers.Authorization = `Bearer ${newAccessToken}`;
+
+//         onTokenRefreshed(newAccessToken); // 첫 요청이 아닌 다른 쌓여있던 요청 다시 요청보내기
+//         refreshSubscribers = []; // 요청 배열 초기화
+//         // console.log("첫 요청도 다시 요청!");
+//         return axios(originalReq); // 첫 요청 다시 요청
+//       }
+//       // 토큰을 생성중일때  (if문을 passing 한 요청들은 )
+//       const retryOriginalRequest = new Promise((resolve) => {
+//         addRefreshSubscriber((accessToken) => {
+//           originalReq.headers.Authorization = "Bearer " + accessToken;
+//           resolve(axios(originalReq));
+//         });
+//       });
+//       return retryOriginalRequest; // 모아둔 요청 재실행
+//     }
+//     return Promise.reject(error);
+//   }
+// );
