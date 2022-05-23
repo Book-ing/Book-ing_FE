477029c5947739b2884dcb3a80c28d370c3e7107
Minwoo Park <minwoopark@Minwoos-MacBook-Air.local>
2022-05-23 18:52:54 +0900

    fix(mainpage/view): fix each sections & width
    mainpage의 각 섹션과 전체 넓이 수정완료에 대한 커밋입니다.
    
    2022.05.23 actual project: book-ing
    commit by Minwoo Park

--- src/components/mainSearch/SearchBar.jsx
+++ src/components/mainSearch/SearchBar.jsx
@@ -1,31 +1,34 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 import { Link, useHistory } from "react-router-dom";
 import { useDispatch } from "react-redux";
 
-import styled from 'styled-components';
+import styled from "styled-components";
 import SearchIcon from "@mui/icons-material/Search";
+
+// themes
+import flex from "../../themes/flex";
 
 const SearchBar = ({ getSearchCrew }) => {
   const dispatch = useDispatch();
   const history = useHistory();
-  
+
   // 검색항목 관리 State
-  const [searchCrew, setSearchCrew] = useState('');
-  
+  const [searchCrew, setSearchCrew] = useState("");
+
   // 검색값 전달하기 위한 함수 선언 (수정해야할 사항이 있음 로직 불안정)
-  const sendSearchCrew = (e) => { 
-      e.preventDefault();
-      if (!e.currentTarget.value || e.currentTarget.value === '') 
+  const sendSearchCrew = (e) => {
+    e.preventDefault();
+    if (!e.currentTarget.value || e.currentTarget.value === "")
       // console.log(searchCrew);
       getSearchCrew(searchCrew, dispatch, history);
-      // history.push("/search");
-      else alert('검색어를 입력해주세요');
-  }
+    // history.push("/search");
+    else alert("검색어를 입력해주세요");
+  };
 
   // const handleKeyPress = (e) => {
   //   if(e.key === 'Enter') {
   //     e.preventDefault();
-  //     if (!e.currentTarget.value || e.currentTarget.value === '') 
+  //     if (!e.currentTarget.value || e.currentTarget.value === '')
   //     // console.log(searchCrew);
   //     getSearchCrew(searchCrew);
   //   }
@@ -35,34 +38,37 @@ const SearchBar = ({ getSearchCrew }) => {
   return (
     <React.Fragment>
       <form onSubmit={sendSearchCrew}>
-      <StSearchBox>
-      <StInputLine>
-        <StInput
-          name="title"
-          placeholder="원하는 모임을 검색해주세요."
-          value={searchCrew}
-          onChange={e => setSearchCrew(e.currentTarget.value)}
-          // onKeyPress={handleKeyPress}
-        />
-        {/* <StSearchBtn onClick={sendSearchCrew}> */}
-        <Link to="/search">
-            <SearchIcon fontSize="large" />
-          {/* <SearchIcon fontSize="large" /> */}
-          </Link>
-        {/* </StSearchBtn> */}
-      </StInputLine>
-      </StSearchBox>
+        <StSearchBox>
+          <StInputLine>
+            <StInput
+              name="title"
+              placeholder="원하는 모임을 검색해주세요."
+              value={searchCrew}
+              onChange={(e) => setSearchCrew(e.currentTarget.value)}
+              // onKeyPress={handleKeyPress}
+            />
+            {/* <StSearchBtn onClick={sendSearchCrew}> */}
+            <IconBox>
+              <Link to="/search">
+                <SearchIcon fontSize="large" />
+                {/* <SearchIcon fontSize="large" /> */}
+              </Link>
+            </IconBox>
+            {/* </StSearchBtn> */}
+          </StInputLine>
+        </StSearchBox>
       </form>
-      </React.Fragment>
+    </React.Fragment>
   );
 };
 
 export default SearchBar;
 
 const StSearchBox = styled.div`
-  display: flex;
-  width: 1440px;
-  margin-left: 130px;
+  ${flex("center", "center", false)}
+  max-width: 1000px;
+  margin: auto;
+  margin-top: 40px;
 `;
 
 const StSearchBtn = styled.button`
@@ -70,18 +76,21 @@ const StSearchBtn = styled.button`
 `;
 
 const StInputLine = styled.div`
-  width: 1045px;
+  ${flex("between")}
+  width: 100%;
   height: 35px;
-  margin-top: 62px;
   align-items: center;
   border-radius: 5px;
   border: 1px solid var(--point);
 `;
 
 const StInput = styled.input`
-  width: 1000px;
+  min-width: 200px;
   height: 35px;
   background-color: transparent;
-  /* border-radius: 5px; */
-  /* border: 1px solid var(--point); */
+`;
+
+const IconBox = styled.div`
+  margin-top: 5px;
+  margin-right: 10px;
 `;
--- src/pages/Main.jsx
+++ src/pages/Main.jsx
@@ -105,121 +105,129 @@ const Main = (props) => {
           </Modal>
         </ModalBtnGrid>
 
-        <StCrewTitle>
-          <Elchip shape="Fill" width="96px" height="35px">
-            <Eltext type="sub_2_bold" color="white">
-              내 모임
-            </Eltext>
-          </Elchip>
-        </StCrewTitle>
-
         {/* <MyCrewGrid> */}
-        <CardGrid>
-          {JSON.stringify(__listMyMeeting) === "{}" ? (
-            <StMyCrew>
-              <Eltext type="head_6_bold" color="rgba(40, 34, 36, 0.5)">
-                마음의 양식을 쌓고 싶다면 모임 생성 / 참가를 해 볼까요?
+        <MyCrewSection>
+          <StCrewTitle>
+            <Elchip shape="Fill" width="96px" height="35px">
+              <Eltext type="sub_2_bold" color="white">
+                내 모임
               </Eltext>
-            </StMyCrew>
-          ) : (
-            __listMyMeeting.map((p, idx) => {
-              return <Cards key={idx} {...p} />;
-            })
-          )}
-        </CardGrid>
+            </Elchip>
+          </StCrewTitle>
+          <CardGrid>
+            {JSON.stringify(__listMyMeeting) === "{}" ? (
+              <StMyCrew>
+                <Eltext type="head_6_bold" color="rgba(40, 34, 36, 0.5)">
+                  마음의 양식을 쌓고 싶다면 모임 생성 / 참가를 해 볼까요?
+                </Eltext>
+              </StMyCrew>
+            ) : (
+              __listMyMeeting.map((p, idx) => {
+                return <Cards key={idx} {...p} />;
+              })
+            )}
+          </CardGrid>
+        </MyCrewSection>
 
         {/* </MyCrewGrid> */}
+
+        <CrewGroupGrid>
+          <GoSearchBtnGrid>
+            <GoSearchBtn
+              shape="brown-outline"
+              onClick={() => {
+                mainActions.getSearchCrew("", dispatch, history);
+              }}
+            >
+              전체 보기
+            </GoSearchBtn>
+          </GoSearchBtnGrid>
+          <CrewListToday>
+            <StCrewTitle>
+              <Elchip shape="Fill" width="170px" height="35px">
+                <Eltext type="sub_2_bold" color="white">
+                  오늘 스터디하는 모임
+                </Eltext>
+              </Elchip>
+            </StCrewTitle>
+            <GroupGrid>
+              <CardGrid>
+                {__listTodayMeeting.map((p, idx) => {
+                  return (
+                    <CardGrid key={idx}>
+                      <Cards {...p} />
+                    </CardGrid>
+                  );
+                })}
+              </CardGrid>
+            </GroupGrid>
+          </CrewListToday>
+          <CrewListRecommend>
+            <StCrewTitle>
+              <Elchip shape="Fill" width="120px" height="35px">
+                <Eltext type="sub_2_bold" color="white">
+                  추천 모임
+                </Eltext>
+              </Elchip>
+            </StCrewTitle>
+            <GroupGrid>
+              <CardGrid>
+                <Swiper
+                  slidesPerView={3}
+                  spaceBetween={10}
+                  slidesPerGroup={3}
+                  loop={true}
+                  loopFillGroupWithBlank={true}
+                  navigation={true}
+                  autoplay={{ delay: 7000 }}
+                  modules={[Navigation, Autoplay]}
+                  className="mySwiper"
+                >
+                  {__listRecommendMeeting.map((p, idx) => {
+                    return (
+                      <SwiperSlide key={idx}>
+                        <Card key={idx} {...p} />
+                      </SwiperSlide>
+                    );
+                  })}
+                </Swiper>
+              </CardGrid>
+            </GroupGrid>
+          </CrewListRecommend>
+          <CrewListNewest>
+            <StCrewTitle>
+              <Elchip shape="Fill" width="120px" height="35px">
+                <Eltext type="sub_2_bold" color="white">
+                  딱-끈한 모임
+                </Eltext>
+              </Elchip>
+            </StCrewTitle>
+            <GroupGrid>
+              <CardGrid>
+                <Swiper
+                  slidesPerView={3}
+                  spaceBetween={10}
+                  slidesPerGroup={3}
+                  loop={true}
+                  loopFillGroupWithBlank={true}
+                  navigation={true}
+                  autoplay={{ delay: 7000 }}
+                  modules={[Navigation, Autoplay]}
+                  className="mySwiper"
+                >
+                  {__listNewMeeting.map((p, idx) => {
+                    return (
+                      <SwiperSlide key={idx}>
+                        <Card key={idx} {...p} />
+                      </SwiperSlide>
+                    );
+                  })}
+                </Swiper>
+              </CardGrid>
+            </GroupGrid>
+          </CrewListNewest>
+        </CrewGroupGrid>
       </Container>
-      <CrewGroupGrid>
-        <GoSearchBtnGrid>
-          <GoSearchBtn
-            shape="brown-outline"
-            onClick={() => {
-              mainActions.getSearchCrew("", dispatch, history);
-            }}
-          >
-            전체 보기
-          </GoSearchBtn>
-        </GoSearchBtnGrid>
-        <TitleGridA>
-          <Elchip shape="Fill" width="170px" height="35px">
-            <Eltext type="sub_2_bold" color="white">
-              오늘 스터디하는 모임
-            </Eltext>
-          </Elchip>
-        </TitleGridA>
-        <GroupGrid>
-          <CardGrid>
-            {__listTodayMeeting.map((p, idx) => {
-              return (
-                <CardGrid key={idx}>
-                  <Cards {...p} />
-                </CardGrid>
-              );
-            })}
-          </CardGrid>
-        </GroupGrid>
-        <StCrewTitle>
-          <Elchip shape="Fill" width="120px" height="35px">
-            <Eltext type="sub_2_bold" color="white">
-              추천 모임
-            </Eltext>
-          </Elchip>
-        </StCrewTitle>
-        <GroupGrid>
-          <CardGrid>
-            <Swiper
-              slidesPerView={3}
-              spaceBetween={10}
-              slidesPerGroup={3}
-              loop={true}
-              loopFillGroupWithBlank={true}
-              navigation={true}
-              autoplay={{ delay: 7000 }}
-              modules={[Navigation, Autoplay]}
-              className="mySwiper"
-            >
-              {__listRecommendMeeting.map((p, idx) => {
-                return (
-                  <SwiperSlide key={idx}>
-                    <Card key={idx} {...p} />
-                  </SwiperSlide>
-                );
-              })}
-            </Swiper>
-          </CardGrid>
-        </GroupGrid>
-        <StCrewTitle>
-          <Elchip shape="Fill" width="120px" height="35px">
-            <Eltext type="sub_2_bold" color="white">
-              딱-끈한 모임
-            </Eltext>
-          </Elchip>
-        </StCrewTitle>
-        <GroupGrid>
-          <CardGrid>
-            <Swiper
-              slidesPerView={3}
-              spaceBetween={10}
-              slidesPerGroup={3}
-              loop={true}
-              loopFillGroupWithBlank={true}
-              navigation={true}
-              autoplay={{ delay: 7000 }}
-              modules={[Navigation, Autoplay]}
-              className="mySwiper"
-            >
-              {__listNewMeeting.map((p, idx) => {
-                return (
-                  <SwiperSlide key={idx}>
-                    <Card key={idx} {...p} />
-                  </SwiperSlide>
-                );
-              })}
-            </Swiper>
-          </CardGrid>
-        </GroupGrid>
-      </CrewGroupGrid>
     </>
   );
 };
@@ -240,8 +248,8 @@ const style = {
 };
 
 const ModalOpenBtn = styled(Elbutton)`
-  width: 147px;
-  height: 35px;
+  width: 120px;
+  height: 30px;
   box-shadow: 0 4px 4px rgba(0, 0, 0, 0.25);
   border-radius: 5px;
 `;
@@ -253,10 +261,9 @@ const ModalCloseBtn = styled.button`
 `;
 
 const ModalBtnGrid = styled.div`
-  display: flex;
-  justify-content: flex-end;
-  padding: 62px 73px 0px 0px;
-  /* border: 1px solid black; */
+  ${flex("end")}
+  max-width: 1000px;
+  margin: 80px auto;
 `;
 
 const GoSearchBtn = styled(Elbutton)`
@@ -267,88 +274,74 @@ const GoSearchBtn = styled(Elbutton)`
 `;
 
 const GoSearchBtnGrid = styled.div`
-  display: flex;
-  justify-content: flex-end;
-  padding: 62px 73px 0px 0px;
+  ${flex("end")}
+  max-width: 1000px;
+  margin: auto;
+  padding: 40px 0 20px 0;
 `;
 
 const StCrewTitle = styled.div`
   width: 100%;
   height: 45px;
-  margin: 20px 0 0 15px;
-  /* border: 1px solid #815854; */
-`;
-
-const TitleGridA = styled.div`
-  width: 100%;
-  height: 45px;
-  margin-left: 15px;
-  /* border: 1px solid #815854; */
+  margin: auto;
+  padding: 0 65px;
+  margin-bottom: 40px;
 `;
 
 const Container = styled.div`
+  width: 100%;
+  max-width: 1200px;
   margin: auto;
-  padding-left: 80px;
-  width: 95vw;
-  max-width: 1440px;
-  height: 700px;
-  box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
-  /* border: 1px solid black; */
+`;
+
+const MyCrewSection = styled.div`
+  ${flex("start", "center", false)}
+  width: 100%;
+  max-height: 600px;
+  padding: 20px 0;
+  background-color: var(--main);
 `;
 
 const CardGrid = styled.div`
   display: flex;
   width: 1200px;
   height: 610px;
-  margin: 10px 30px 0 0px;
-  /* border: 1px solid black; */
 `;
 
 const StMyCrew = styled.div`
   ${flex("center", "center", true)}
   width: 100%;
-  /* margin-left: 70px; */
   color: rgba(40, 34, 36, 0.5);
-  /* border: 1px solid black; */
 `;
 
 const CrewGroupGrid = styled.div`
-  padding: 20px 0 0 80px;
-  margin: auto;
-  width: 95vw;
-  max-width: 1440px;
-  height: 1515px;
-  box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
+  width: 100%;
+  height: 100%;
 `;
 
 const GroupGrid = styled.div`
   display: flex;
-  /* margin-left: 40px; */
-`;
-
-const StSearchBox = styled.div`
-  display: flex;
-  width: 1440px;
-  margin-left: 130px;
 `;
 
 const StSearchBtn = styled.button`
-  vertical-align: middle;
+  width: 100%;
 `;
 
-const StInputLine = styled.div`
-  width: 1045px;
-  height: 35px;
-  margin-top: 62px;
-  align-items: center;
-  border-radius: 5px;
-  border: 1px solid var(--point);
+const CrewListToday = styled.div`
+  width: 100%;
+  max-height: 610px;
+  margin-top: 120px;
+  background: linear-gradient(to top, #839893, var(--white));
 `;
-
-const StInput = styled.input`
-  width: 1000px;
-  height: 35px;
-  background-color: transparent;
-  /* border-radius: 5px; */
-  /* border: 1px solid var(--point); */
+const CrewListRecommend = styled.div`
+  width: 100%;
+  max-height: 610px;
+  margin-top: 120px;
+  background: linear-gradient(to top, #c9998d, var(--white));
+`;
+const CrewListNewest = styled.div`
+  width: 100%;
+  max-height: 610px;
+  margin: 120px 0 100px 0;
+  background: linear-gradient(to top, #ede1d3, var(--white));
 `;
